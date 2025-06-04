815088448187361dff343e712a242b3c75af8d91
jemoyy <138149423+jemoyy@users.noreply.github.com>
2025-06-04 20:40:34 +0800

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showInfo() {
-    alert("EcoClean is a plant-based, non-toxic dishwashing liquid designed for families who care about the planet and their health.");
+    alert("MAXGLOW is a plant-based, non-toxic dishwashing liquid designed for families who care about the planet and their health.");
   }
   function buyNow() {
-    window.location.href = ''; // Replace with your actual product URL
+    window.location.href = '';
   }
