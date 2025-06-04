aefac3849a63c0b0e14522450ef9deb8fc1864cd
jemoyy <138149423+jemoyy@users.noreply.github.com>
2025-06-04 20:26:15 +0800

    script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function showInfo() {
     alert("EcoClean is a plant-based, non-toxic dishwashing liquid designed for families who care about the planet and their health.");
   }
   function buyNow() {
-    window.location.href = 'https://pornhub.com'; // Replace with your actual product URL
+    window.location.href = ''; // Replace with your actual product URL
   }
